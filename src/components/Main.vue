d7c3f5f962951c7ebd1c4baa286915abc0ff9edc
Vitali Zaikov <zaikov@ut.ee>
2022-11-20 00:00:19 +0200

    proper cycle

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,24 +1,23 @@
 <template>
-  <div>
-    <Post v-for="post in $store.state.posts" :post="post" :key="post.id"/>
-  </div>
+    <div>
+        <Post v-for="post in $store.state.posts" :post="post" :key="post.id" />
+    </div>
 </template>
 
 <script>
 import Post from "@/components/Post";
 
 export default {
-  name: "Main",
-  components: {Post}
-}
+    name: "Main",
+    components: { Post },
+};
 </script>
 
 <style scoped>
 div {
-  width: 60%;
-  display: flex;
-  flex-direction: column;
-  gap: 1vh;
+    width: 60%;
+    display: flex;
+    flex-direction: column;
+    gap: 1vh;
 }
-
 </style>
