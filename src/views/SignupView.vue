cd4a2e72d686fac5bc011b8b5ec6f218cb05e17b
Vitali Zaikov <zaikov@ut.ee>
2022-11-19 15:48:43 +0200

    no signup yet

--- src/views/SignupView.vue
+++ src/views/SignupView.vue
@@ -7,12 +7,11 @@
 <script>
 import Header from '@/components/Header.vue';
 import Footer from '@/components/Footer.vue';
-import SignUp from '@/components/SignUp.vue';
 
 export default {
   name: 'SignUpView',
   components: {
-	Header, Footer, SignUp
+	Header, Footer
   }
 }
 </script>
